9337d9c0460a1edbbe8a61d5254b750d3d4ab9e6
nils knof <nilsknof19@gmail.com>
2023-10-09 11:13:01 +0200

    run scripts

---
+++ eval_rsl.py
@@ -0,0 +1,260 @@
+import csv
+import json
+
+from libs import xcsf
+import click
+import os
+import numpy as np
+from sklearn.pipeline import make_pipeline
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.metrics import mean_squared_error
+import mlflow
+import seaborn as sns
+import pandas as pd
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument('NPZFILE')
+def run(npzfile):
+    mlflow.set_tracking_uri('data/results/')
+    df = mlflow.search_experiments("run")
+    pass
+
+
+# copied from the run-rsl-bench repository: https://doi.org/10.5281/zenodo.7915821
+@cli.command()
+@click.argument("PATH")
+def datasets(path):
+    print("test")
+    fnames = os.listdir(path)
+
+    def entry(fname):
+        data = np.load(f"{path}/{fname}", allow_pickle=True)
+        X = data["X"]
+        N, DX = X.shape
+        centers = data["centers"]
+        K = len(centers)
+
+        return dict(N=N,
+                    DX=DX,
+                    K=K,
+                    linear_model_mse=data["linear_model_mse"][()],
+                    linear_model_rsquared=data["linear_model_rsquared"][()],
+                    rsl_model_mse=data["rsl_model_mse"][()],
+                    rsl_model_rsquared=data["rsl_model_rsquared"][()])
+
+    index = ["DX", "K", "N"]
+    df = pd.DataFrame(map(entry, fnames))
+
+    summary = df.set_index(index).sort_index().index
+    print(np.unique(summary, return_counts=True))
+
+    fig, ax = plt.subplots(2, layout="constrained", figsize=(10, 3 * 10))
+    sns.boxplot(
+        data=df,
+        x="DX",
+        hue="K",
+        y="linear_model_mse",
+        ax=ax[0],
+    )
+    sns.boxplot(
+        data=df,
+        x="DX",
+        hue="K",
+        y="rsl_model_mse",
+        ax=ax[1],
+    )
+    sns.stripplot(data=df,
+                  x="DX",
+                  hue="K",
+                  y="linear_model_mse",
+                  ax=ax[0],
+                  dodge=True,
+                  palette="dark:0",
+                  size=3)
+    sns.stripplot(data=df,
+                  x="DX",
+                  hue="K",
+                  y="rsl_model_mse",
+                  ax=ax[1],
+                  dodge=True,
+                  palette="dark:0",
+                  size=3)
+    plt.show()
+
+    means = df.groupby(index).mean().round(2)
+    n_datasets = df.groupby(index).apply(len).unique()
+    print(f"Mean of the {n_datasets} datasets per {index} combination")
+    print(means[["linear_model_mse", "rsl_model_mse"]].to_latex())
+
+    # A more space saving representation.
+    means = means[["linear_model_mse", "rsl_model_mse"
+                   ]].reset_index().set_index(["DX", "N", "K"]).unstack("K")
+    print(means.to_latex())
+
+    import IPython
+    IPython.embed(banner1="")
+    import sys
+    sys.exit(1)
+    # consider running `globals().update(locals())` in the shell to fix not being
+    # able to put scopes around variables
+
+
+cli.add_command(run)
+
+cli.add_command(datasets)
+if __name__ == '__main__':
+    # cli()
+    # copied from the run-rsl-bench repository: https://doi.org/10.5281/zenodo.7915821
+    # mlflow.set_tracking_uri('data/results/')
+    # df = mlflow.search_runs(experiment_names=["rsl-K5-DX5-N1000"])
+    # df = df[df.status == "FINISHED"]
+    # # pass
+    # index = ["params.data.DX", "params.data.K", "tags.mlflow.runName"]
+    # metrics = ["metrics.mse.test.csr", "metrics.mse.test.ubr", "metrics.mse.test.mmr", "metrics.mse.test.mpr"]
+    # df['tags.mlflow.runName'] = [s[-1] for s in df['tags.mlflow.runName']]
+    # df_metrics = df.reset_index()[index + metrics]
+    #
+    pretty = {
+        "params.data.DX": "$\mathcal{D}_\mathcal{X}$",
+        "params.data.K": "$K$",
+        "tags.mlflow.runName": "Data Seed"
+    }
+    #
+    # df_metrics = df_metrics.set_index(index).stack().reset_index().rename(
+    #     columns={
+    #                 0: "Test MSE",
+    #                 "level_3": "Algorithm",
+    #             } | pretty)
+    #
+    labels = {
+        "metrics.mse.test.ubr": "UBR",
+        "metrics.mse.test.csr": "CSR",
+        "metrics.mse.test.mmr": "MMR",
+        "metrics.mse.test.mpr": "MPR",
+    }
+    #
+    # df_metrics["Algorithm"] = df_metrics["Algorithm"].apply(
+    #     lambda s: labels[s])
+
+    df_metrics = pd.read_csv('Auswertung/results_test_mse.CSV')
+    dx = 50
+    k = 'all'
+    df_metrics = df_metrics[df_metrics["$\mathcal{D}_\mathcal{X}$"] == dx]
+    # df_metrics = df_metrics[df_metrics["$K$"] == k]
+
+    df_metrics_mean = df_metrics.groupby(["$\mathcal{D}_\mathcal{X}$","$K$", "Data Seed", "Algorithm"])["Test MSE"].mean().reset_index()
+    df_metrics_min = df_metrics.groupby(["$\mathcal{D}_\mathcal{X}$","$K$", "Data Seed", "Algorithm"])["Test MSE"].min().reset_index()
+    df_metrics_low = df_metrics.groupby(["$\mathcal{D}_\mathcal{X}$","$K$", "Data Seed", "Algorithm"])["Test MSE"].quantile(.25).reset_index()
+    df_metrics_med = df_metrics.groupby(["$\mathcal{D}_\mathcal{X}$","$K$", "Data Seed", "Algorithm"])["Test MSE"].quantile(.5).reset_index()
+    df_metrics_high = df_metrics.groupby(["$\mathcal{D}_\mathcal{X}$","$K$", "Data Seed", "Algorithm"])["Test MSE"].quantile(.75).reset_index()
+
+    g = sns.FacetGrid(data=df_metrics_mean,
+                      row=pretty["params.data.DX"],
+                      col=pretty["params.data.K"],
+                      hue="Algorithm",
+                      hue_order=list(labels.values()),
+                      sharey=False,
+                      margin_titles=True)
+
+    g.map(
+        sns.pointplot,
+        pretty["tags.mlflow.runName"],
+        "Test MSE",
+        order=np.sort(df_metrics[pretty["tags.mlflow.runName"]].unique()),
+        errorbar=("ci", 95),
+        capsize=0.3,
+        errwidth=2.0,
+    )
+    g.add_legend()
+    plt.savefig(f"Auswertung/plots/dim_{dx}_pop_{k}_mean.pdf")
+
+    g = sns.FacetGrid(data=df_metrics_min,
+                      row=pretty["params.data.DX"],
+                      col=pretty["params.data.K"],
+                      hue="Algorithm",
+                      hue_order=list(labels.values()),
+                      sharey=False,
+                      margin_titles=True)
+
+    g.map(
+        sns.pointplot,
+        pretty["tags.mlflow.runName"],
+        "Test MSE",
+        order=np.sort(df_metrics[pretty["tags.mlflow.runName"]].unique()),
+        errorbar=("ci", 95),
+        capsize=0.3,
+        errwidth=2.0,
+    )
+    g.add_legend()
+    plt.savefig(f"Auswertung/plots/dim_{dx}_pop_{k}_min.pdf")
+
+    g = sns.FacetGrid(data=df_metrics_low,
+                      row=pretty["params.data.DX"],
+                      col=pretty["params.data.K"],
+                      hue="Algorithm",
+                      hue_order=list(labels.values()),
+                      sharey=False,
+                      margin_titles=True)
+
+    g.map(
+        sns.pointplot,
+        pretty["tags.mlflow.runName"],
+        "Test MSE",
+        order=np.sort(df_metrics[pretty["tags.mlflow.runName"]].unique()),
+        errorbar=("ci", 95),
+        capsize=0.3,
+        errwidth=2.0,
+    )
+    g.add_legend()
+    plt.savefig(f"Auswertung/plots/dim_{dx}_pop_{k}_low.pdf")
+
+    g = sns.FacetGrid(data=df_metrics_med,
+                      row=pretty["params.data.DX"],
+                      col=pretty["params.data.K"],
+                      hue="Algorithm",
+                      hue_order=list(labels.values()),
+                      sharey=False,
+                      margin_titles=True)
+
+    g.map(
+        sns.pointplot,
+        pretty["tags.mlflow.runName"],
+        "Test MSE",
+        order=np.sort(df_metrics[pretty["tags.mlflow.runName"]].unique()),
+        errorbar=("ci", 95),
+        capsize=0.3,
+        errwidth=2.0,
+    )
+    g.add_legend()
+    plt.savefig(f"Auswertung/plots/dim_{dx}_pop_{k}_med.pdf")
+
+    g = sns.FacetGrid(data=df_metrics_high,
+                      row=pretty["params.data.DX"],
+                      col=pretty["params.data.K"],
+                      hue="Algorithm",
+                      hue_order=list(labels.values()),
+                      sharey=False,
+                      margin_titles=True)
+
+    g.map(
+        sns.pointplot,
+        pretty["tags.mlflow.runName"],
+        "Test MSE",
+        order=np.sort(df_metrics[pretty["tags.mlflow.runName"]].unique()),
+        errorbar=("ci", 95),
+        capsize=0.3,
+        errwidth=2.0,
+    )
+    g.add_legend()
+    plt.savefig(f"Auswertung/plots/dim_{dx}_pop_{k}_high.pdf")
+
+
+    pass
--- run_realdata.py
+++ run_realdata.py
@@ -1,5 +1,5 @@
 import json
-
+import numpy
 from libs import xcsf
 import click
 import os
@@ -10,6 +10,7 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import mlflow
+import csv
 
 
 def get_train(data):
@@ -42,14 +43,12 @@ def cli():
               "--endseed",
               type=click.IntRange(min=0),
               default=0,)
-@click.option("--experiment-name", type=str, required=True)
 @click.argument('NPZFILE')
 @click.pass_context
-def runmany(ctx, startseed, endseed, experiment_name, npzfile):
+def runmany(ctx, startseed, endseed, npzfile):
     for seed in range(startseed, endseed + 1):
         ctx.invoke(
             run,
-            experiment_name=experiment_name,
             npzfile=npzfile,
             seed=seed,
         )
@@ -64,8 +63,8 @@ def runmany(ctx, startseed, endseed, experiment_name, npzfile):
 def run(npzfile, seed):
     y_dim = 1
     n_actions = 1
-    alpha = 0.1
-    beta = 0.2
+    alpha = 0.5
+    beta = 0.0052
     delta = 0.1
     e0 = 0.01
     init_error = 0
@@ -74,7 +73,7 @@ def run(npzfile, seed):
     nu = 5
     omp_num_threads = 8
     perf_trials = 1000
-    max_trials = 100000
+    max_trials = 50000
     pop_init = True
     pop_size = 5000
     random_state = seed
@@ -88,7 +87,7 @@ def run(npzfile, seed):
         "theta_ea": 25,
         "lambda": 2,
         "p_crossover": 0.8,
-        "err_reduc": 0.25,
+        "err_reduc": 0.5,
         "fit_reduc": 0.1,
         "subsumption": True,
         "pred_reset": False,
@@ -96,6 +95,7 @@ def run(npzfile, seed):
     action = {
         "type": "integer",
     }
+
     conditionArgs = {
         "min": -1.0,
         "max": 1.0,
@@ -111,13 +111,29 @@ def run(npzfile, seed):
         }
     }
 
-    mlflow.set_tracking_uri('data/test/')
-    mlflow.set_experiment("rsl")
+    mlflow.set_tracking_uri('data/realworld')
+    mlflow.set_experiment("real_data_sd")
     print("Tracking URI:", mlflow.get_tracking_uri())
     with mlflow.start_run(run_name=npzfile[5:-4]) as run:
         # training data
-        data = np.load(npzfile)
-        X, y = get_train(data)
+        with open(npzfile, 'r') as f:
+            # reader = csv.reader(f, delimiter=',') //real_data_physicochemical_properties
+            reader = csv.reader(f, delimiter=',')
+            data = list(reader)
+        data = np.array(data).astype(float)
+        np.random.seed(random_state)
+        np.random.shuffle(data)
+        split = int(data.shape[0] * 0.75)
+        train, test = data[:split], data[split:]
+        # real_data_winequality_red
+        X, y = train[:, :-1], train[:, -1:]
+        y = np.log(y, where=y!=0.0)
+        X_test, y_test = test[:, :-1], test[:, -1:]
+        y_test = np.log(y_test, where=y_test!=0.0)
+        # real_data_physicochemical_properties
+        # X, y = train[:, 1:], train[:, :1]
+        # X_test, y_test = test[:, 1:], test[:, :1]
+
         N, DX = X.shape
         mlflow.log_params({
             "data.N": N,
@@ -125,20 +141,6 @@ def run(npzfile, seed):
             "seed": seed,
             "pop_size": pop_size,
             "max_trials": max_trials,
-        })
-
-        # test data
-        X_test, y_test = get_test(data)
-
-        K = len(data["centers"])
-        mlflow.log_params({
-            "data.K": K,
-            "data.linear_model_mse":        data["linear_model_mse"],
-            "data.linear_model_mae":        data["linear_model_mae"],
-            "data.linear_model_rsquared":   data["linear_model_rsquared"],
-            "data.rsl_model_mse":           data["rsl_model_mse"],
-            "data.rsl_model_mae":           data["rsl_model_mae"],
-            "data.rsl_model_rsquared":      data["rsl_model_rsquared"],
         })
 
         def eval_model(model, label):
@@ -304,6 +306,7 @@ def run(npzfile, seed):
         eval_model(model_csr, "csr")
         eval_model(model_mmr, "mmr")
         eval_model(model_mpr, "mpr")
+
 
 cli.add_command(run)
 
